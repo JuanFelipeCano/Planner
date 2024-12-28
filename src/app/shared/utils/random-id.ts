ef993e988b9b19b9e646c013190b9fea6e8d8f29
Juan Betancur <juanfelipebetancurcano@gmail.com>
2024-12-28 01:31:33 -0500

    resolve security hotspots

--- src/app/shared/utils/random-id.ts
+++ src/app/shared/utils/random-id.ts
@@ -1,9 +1,14 @@
 export const randomId = (key: string = '', length: number = 10): string => {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let id = '';
+  // Create a buffer to hold random values
+  const randomValues = new Uint8Array(length);
+  // Fill the buffer with random values
+  window.crypto.getRandomValues(randomValues);
 
+  let id = '';
   for (let i = 0; i < length; i++) {
-    id += chars.charAt(Math.floor(Math.random() * chars.length));
+    // Use the random value from the array to pick a character from `chars`
+    id += chars.charAt(randomValues[i] % chars.length);
   }
 
   return `${ key }-${ id }`;
